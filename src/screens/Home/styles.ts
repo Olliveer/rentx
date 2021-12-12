27c1b9c00438c22b20c4be34be2e987d61a5605b
José Oliveira <j.oliveira90@live.com>
2021-12-12 17:03:21 -0300

    add assets and header component

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,16 +1,31 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-const Title = styled.Text`
-  font-size: 24px;
-  font-family: ${({ theme }) => theme.fonts.secondary_600};
+const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+  padding: 32px 24px;
 `;
 
-export { Container, Title };
+const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export { Container, Header, TotalCars, HeaderContent };
