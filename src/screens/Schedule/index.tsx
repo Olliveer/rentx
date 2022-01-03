5578199882a557d1e03d7b083219922b5f231e67
José Oliveira <j.oliveira90@live.com>
2022-01-02 21:21:05 -0300

    Calendar component

---
+++ src/screens/Schedule/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import {
+  Container,
+  Header,
+  Title,
+  DateInfo,
+  RentalPeriod,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import { StatusBar } from 'react-native';
+import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
+
+export function Schedule() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton color={theme.colors.shape} onPress={() => {}} />
+
+        <Title>
+          Escolha uma{'\n'}data de início e{'\n'}fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ </DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Calendar />
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
---
+++ src/screens/Schedule/styles.ts
@@ -0,0 +1,86 @@
+import styled, { css } from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+type DateValueProps = {
+  selected: boolean;
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+const Header = styled.View`
+  width: 100%;
+  height: 325px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: center;
+  padding: 25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+`;
+
+const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(34)}px;
+`;
+
+const DateInfo = styled.View`
+  width: 30%;
+`;
+
+const RentalPeriod = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 32px 0;
+`;
+
+const DateTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(10)}px;
+`;
+
+const DateValue = styled.Text<DateValueProps>`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(15)}px;
+
+  ${({ selected, theme }) =>
+    !selected &&
+    css`
+      border-bottom-width: 1px;
+      border-bottom-color: ${theme.colors.text};
+      padding-bottom: 5px;
+    `}
+`;
+
+const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingBottom: 24,
+  },
+  showsVerticalScrollIndicator: false,
+})``;
+
+const Footer = styled.View`
+  padding: 24px;
+`;
+
+export {
+  Container,
+  Header,
+  Title,
+  DateInfo,
+  RentalPeriod,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+};
