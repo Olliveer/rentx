d734f2606ffa693bdd40cc7c3cde8ab9bc1da6ea
José Oliveira <j.oliveira90@live.com>
2022-02-27 15:41:09 -0300

    add watermelonDB and create user schema

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+import { Database } from '@nozbe/watermelondb';
+import { User } from './models/User';
+import { schemas } from './schema';
+
+const adapter = new SQLiteAdapter({
+  schema: schemas,
+});
+
+export const database = new Database({
+  adapter,
+  modelClasses: [User],
+});
