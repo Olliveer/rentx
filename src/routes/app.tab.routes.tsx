5269279d945467322c5e9d565f3643866759207b
José Oliveira <j.oliveira90@live.com>
2022-03-12 13:28:24 -0300

    add profile screen

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,6 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { Home } from '../screens/Home';
 import { MyCars } from '../screens/MyCars';
 import { AppStackRoutes } from './app.stack.routes';
 
@@ -9,6 +8,7 @@ import CarSvg from '../assets/car.svg';
 import PeopleSvg from '../assets/people.svg';
 import { useTheme } from 'styled-components';
 import { Platform } from 'react-native';
+import { Profile } from '../screens/Profile';
 
 export type RootStackParamList = {
   HomeTab: undefined;
@@ -44,21 +44,23 @@ export function AppTabRoutes() {
           ),
         }}
       />
-      <Screen
-        name="Profile"
-        component={Home}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <PeopleSvg width={24} height={24} fill={color} />
-          ),
-        }}
-      />
+
       <Screen
         name="MyCars"
         component={MyCars}
         options={{
           tabBarIcon: ({ color }) => (
             <CarSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <PeopleSvg width={24} height={24} fill={color} />
           ),
         }}
       />
---
+++ src/screens/Profile/styles.ts
@@ -0,0 +1,121 @@
+import {
+  BorderlessButton,
+  RectButton,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled, { css } from 'styled-components/native';
+
+type OptionProps = {
+  active: boolean;
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+const Header = styled.View`
+  width: 100%;
+  height: 227px;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  padding: 0px 24px;
+  align-items: center;
+`;
+
+const HeaderTop = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: ${getStatusBarHeight() + 32}px;
+`;
+
+const HeaderTitle = styled.Text`
+  font-size: ${RFValue(25)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+const LogoutButton = styled(BorderlessButton)``;
+
+const PhotoContainer = styled.View`
+  width: 180px;
+  height: 180px;
+
+  border-radius: 90px;
+
+  background-color: ${({ theme }) => theme.colors.shape};
+  margin-top: 48px;
+`;
+
+const Photo = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+`;
+
+const PhotoButton = styled(RectButton)`
+  width: 40px;
+  height: 40px;
+  background-color: ${({ theme }) => theme.colors.main};
+
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+`;
+
+const Content = styled.View`
+  flex: 1;
+  padding: 0px 24px;
+  margin-top: 122px;
+`;
+
+const Options = styled.View`
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.colors.line};
+
+  flex-direction: row;
+  justify-content: space-around;
+
+  margin-bottom: 24px;
+`;
+
+const Option = styled(TouchableOpacity)<OptionProps>`
+  padding-bottom: 14px;
+  ${({ active }) =>
+    active &&
+    css`
+      border-bottom-width: 3px;
+      border-bottom-color: ${({ theme }) => theme.colors.main};
+    `}
+`;
+
+const OptionTitle = styled.Text<OptionProps>`
+  font-size: ${RFValue(20)}px;
+  font-family: ${({ theme, active }) =>
+    active ? theme.fonts.secondary_600 : theme.fonts.secondary_500};
+  color: ${({ theme, active }) =>
+    active ? theme.colors.header : theme.colors.text_detail};
+`;
+
+export {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  LogoutButton,
+  PhotoContainer,
+  Photo,
+  PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
+};
